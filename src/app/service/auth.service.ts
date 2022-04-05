2f94c5e5257547f58197d09afc9b09246352dbd5
unknown <jeferson.aristaque@gmail.com>
2022-04-05 12:17:34 -0300

    Implementando componente Inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -17,8 +18,18 @@ export class AuthService {
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('https://blogdoaristaque.herokuapp.com/usuarios/logar', userLogin)
   }
+
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('https://blogdoaristaque.herokuapp.com/usuarios/cadastrar', user)
+
+  }
+  
+  logado() {
+    let ok = false
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
 
   }
 }
